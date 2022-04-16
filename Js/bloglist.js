f25c272dc071a3fd780e21c1de80660a164fe56a
Vivek Kumar <vivek.network9525@gmail.com>
2022-04-17 00:11:21 +0530

    some changes in post.js

--- Js/bloglist.js
+++ Js/bloglist.js
@@ -18,3 +18,16 @@ function hideModal() {
   // try {
   deleteModal.style.display = "none";
 }
+
+//This is for Showing Post Details in new Page
+function viewPostDetails(id) {
+  var post = document.getElementById(id);
+  var author = post.querySelector(".author-name").innerHTML;
+  var title = post.querySelector(".title").innerHTML;
+  var content = post.querySelector(".post-content").innerHTML;
+  var postUrl =
+    "post.html?author=" + author + "&title=" + title + "&content=" + content;
+
+  window.location.href = postUrl;
+  console.log(postUrl);
+}
